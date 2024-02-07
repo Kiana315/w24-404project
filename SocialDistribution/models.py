0fce5849ff40ebbea9961c08b9d3e02a9680e480
Edennnnnnnnnn <zhou9@ualberta.ca>
2024-02-06 17:00:05 -0700

    init venv and app settings

---
+++ SocialDistribution/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Todo: Fell free to edit, just copy from lab3
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+""" New Codes """
+class Post(models.Model):
+    pass

c2bad9bd6b048319205c3d7a886dc4e18ea05841
Edennnnnnnnnn <zhou9@ualberta.ca>
2024-02-11 15:24:55 -0700

    page urls deployment

--- SocialDistribution/urls.py
+++ SocialDistribution/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 from django.contrib.auth import views as auth_views
-
 
 
 app_name = "SocialDistribution"
 
-""" May edit this part to have new web pages"""
 urlpatterns = [
-    path("", views.IndexView, name="index"),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # path('signup/', views.signup_view, name='signup'),
-] 
+    path("", views.indexView, name="home"),
+    path('admin/', admin.site.urls, name="admin"),
+    path("home/", views.indexView, name="home"),
+    path("login/", views.loginView, name="login"),
+    path("signup/", views.signupView, name="signup"),
+    path("friendPosts/<str:username>/", views.signupView, name="friendPosts"),
+    path("profile/<str:username>/", views.profileView, name="profile"),
+    path("inBox/", views.inboxView, name="inbox"),
+]
 

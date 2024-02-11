aec7f3925e6595cd88108cac2245a83db3ae3594
Kiana Yu <qianya1@ualberta.ca>
2024-02-11 13:31:26 -0700

    Update HTML & urls

--- SocialDistribution/urls.py
+++ SocialDistribution/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 
 app_name = "SocialDistribution"
+
 """ May edit this part to have new web pages"""
 urlpatterns = [
-    path("", views.index, name="index"),
-  
-]
+    path("", views.IndexView, name="index"),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('signup/', views.signup_view, name='signup'),
+] 
+
--- SocialDistribution/views.py
+++ SocialDistribution/views.py
@@ -1,23 +1,21 @@
-from django.shortcuts import render
-
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
-# Todo: Fell free to edit, just copy from lab3
 from .models import User, Post, Comment, Like, Follower
 
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
 
 
 
 
-def index(request):
-    return render(request, "index.html")
+def IndexView(request):
+    return render(request, 'index.html', {'user': request.user})
 
 
+  
 
 
 # # Todo: Fell free to edit all the following codes, just copy from lab3

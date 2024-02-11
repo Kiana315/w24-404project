84977969f782d982796983d170452b16bf740c8d
Edennnnnnnnnn <zhou9@ualberta.ca>
2024-02-11 16:02:07 -0700

    re

--- SocialDistribution/views.py
+++ SocialDistribution/views.py
@@ -25,5 +25,8 @@ def inboxView(request):
     return render(request, 'inbox.html')
 
 
-def friendPostsView(request):
+def friendPostsView(request, username):
+    # user = get_object_or_404(User, username=username)
+    # context = {'profile_user': user}
+    # return render(request, 'profile.html', context)
     return render(request, 'friendPosts.html')

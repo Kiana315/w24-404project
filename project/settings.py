e2effe37bcaa1cc8d55aa7519a0c76c167483a1b
Kiana Yu <qianya1@ualberta.ca>
2024-02-26 14:55:22 -0700

    Modify setting

--- project/settings.py
+++ project/settings.py
@@ -125,7 +125,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'SocialDistribution.User'
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = os.path.join(BASE_DIR, '/login')
+LOGOUT_REDIRECT_URL = '/login/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
